0be04cfacf751f12be41500408a6d8794132d863
Doodle Huang <myjhao@hotmail.com>
2024-05-25 16:12:32 +0800

    open source ss_ana(林槐语录)

---
+++ ss_ana/__init__.py
@@ -0,0 +1,52 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from nonebot import on_command, on_startswith
+from nonebot.adapters import Event
+from nonebot.rule import is_type
+from nonebot.adapters.telegram.event import MessageEvent as TEvent
+from himibot.plugins.keep_safety import is_banned
+from nonebot.adapters.discord import MessageSegment
+from nonebot.adapters.discord.commands import on_slash_command
+from httpx import get, HTTPError
+import yaml
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="ss-ana",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+with open('config.yml', 'r', encoding='utf-8') as f:
+    ss_ana_endpoint = yaml.safe_load(f)['ss_ana_endpoint']
+ss_ana_discord = on_slash_command(name="ss-ana", description='获取一条林槐语录。')
+ss_ana_telegram = on_startswith('/ss-ana', rule=is_type(TEvent))
+ss_ana = on_command("ss-ana", priority=5, aliases={'lhyl', '林槐语录', 'ss'}, block=True)
+
+def get_ss_ana():
+    try:
+        r = get(ss_ana_endpoint)
+        r.raise_for_status()
+        r = r.json()
+        return(r['text'] + '\n（' + r['time'].split('/')[0] + '年' + str(int(r['time'].split('/')[1])) + '月）')
+    except HTTPError as e:
+        return('获取失败：' + str(e))
+    
+@ss_ana_discord.handle()
+async def handle():
+    await ss_ana_discord.send_deferred_response()
+    await ss_ana_discord.finish(MessageSegment.text(get_ss_ana()))
+
+@ss_ana_telegram.handle()
+async def handle(event: TEvent):
+    await ss_ana_telegram.finish(get_ss_ana())
+
+@ss_ana.handle()
+async def handle(event: Event):
+    if event.get_session_id().startswith('group'):
+        group_id = event.get_session_id().split('_')[1]
+        if is_banned(group_id):
+            return
+    await ss_ana.finish(get_ss_ana())
